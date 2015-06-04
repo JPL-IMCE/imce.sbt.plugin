47f5b520d315d9a17c2d350bee3203130d2f5930
Nicolas Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2015-06-04 09:09:28 -0700

    helper methods for specifying dependencies on maven-published git-versioned artifacts.

--- src/main/scala/gov/nasa/jpl/mbee/sbt/MBEEOrganizationInfo.scala
+++ src/main/scala/gov/nasa/jpl/mbee/sbt/MBEEOrganizationInfo.scala
@@ -8,4 +8,32 @@ import sbt._
  * @param name For human consumption
  * @param url
  */
-case class MBEEOrganizationInfo(groupId: String, name: String, url: Option[URL] = None)
+case class MBEEOrganizationInfo(groupId: String, name: String, url: Option[URL] = None) {
+
+  /**
+   * Construct an SBT ModuleID for libraryDependencies for a git-versioned jar artifact published by this organization
+   *
+   * @param artifactId The name of an artifact published by this organization
+   * @param versionPrefix The version prefix, typically, MBEEKeys.mbeeReleaseVersionPrefix.value
+   * @param versionSuffix The version suffix, typically, the 40-character GIT hash
+   * @return an sbt.ModuleID that can be added to libraryDependencies
+   */
+  def mbeeArtifactVersion(artifactId: String, versionPrefix: String, versionSuffix: String): ModuleID = {
+    val version: String = versionPrefix+"-"+versionSuffix
+    groupId %% artifactId % version
+  }
+
+  /**
+   * Construct an SBT ModuleID for libraryDependencies for a git-versioned zip artifact published by this organization
+   *
+   * @param artifactId The name of an artifact published by this organization
+   * @param versionPrefix The version prefix, typically, MBEEKeys.mbeeReleaseVersionPrefix.value
+   * @param versionSuffix The version suffix, typically, the 40-character GIT hash
+   * @return an sbt.ModuleID that can be added to libraryDependencies
+   */
+  def mbeeZipArtifactVersion(artifactId: String, versionPrefix: String, versionSuffix: String): ModuleID = {
+    val version: String = versionPrefix+"-"+versionSuffix
+    groupId %% artifactId % version artifacts Artifact(artifactId, "zip", "zip")
+  }
+
+}
