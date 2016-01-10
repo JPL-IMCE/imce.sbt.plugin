2e3acd214b9564da06e6dc8ce8608a656c70180e
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2016-01-10 01:30:10 +0000

    This commit should be signed.

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@
 object Versions {
 
   // this project's version
-  val version = "1.25"
+  val version = "1.26"
 
   // https://github.com/Banno/sbt-license-plugin
   val sbt_license_plugin = "0.1.5"
