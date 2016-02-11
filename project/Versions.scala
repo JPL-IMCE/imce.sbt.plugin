0868f5a095f576b2455cbc51a4d33238c4ce19bb
Tyler Ryan <tyler.j.ryan@jpl.nasa.gov>
2016-02-11 22:34:51 +0000

    Increment version for test build

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@
 object Versions {
 
   // this project's version
-  val version = "2.2.1"
+  val version = "2.2.2"
 
   // https://github.com/Banno/sbt-license-plugin
   val sbt_license_plugin = "0.1.5"
@@ -62,3 +62,4 @@ object Versions {
   val better_files = "2.14.0"
 
 }
+
